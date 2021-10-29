95358a55372de5fd2ee13f3974f1599af4f856ed
nikasparks <nikasparks@mac.com>
2021-10-29 00:47:33 -0600

    n

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -2,19 +2,15 @@
 const images2load = document.querySelectorAll("img[data-src]");
 
 //optinal images options in observer
-// const imgoptions = {
-//     threshold: 0,
-//     rootMargin: "0px 0px 50px 0px"
-// };
+const imgoptions = {
+    threshold: 100,
+    rootMargin: "500px"
+};
 
 const loadimg = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {image.removeAttribute('data-src');};
 };
-
-    // images2load.forEach((img) => {
-    //     loadimg(img);
-    // });
 
 if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
