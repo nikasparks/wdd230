f291e59e7170a1b291ea1ed53b61dc5d5638bc3f
nikasparks <nikasparks@mac.com>
2021-11-13 23:33:44 -0700

    v

--- lesson9/js/towninfo.js
+++ lesson9/js/towninfo.js
@@ -6,8 +6,10 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
-    prophets.forEach(prophet => {
+    const townInfo = jsonObject['townInfo'];
+
+    const town = townInfo.filter((town) => town.name === 'Preston' || town.name === 'Fish Haven' || town.name === 'Soda Springs')
+    town.forEach(town => {
         //create the card with the elements for the information
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
@@ -16,11 +18,11 @@ fetch(requestURL)
         let image = document.createElement('img');
 
         //get info from the JSON file
-        h2.innerHTML = `<span class="pName">${prophet.name} ${prophet.lastname}</span>`;
-        birthdate.innerHTML = `<span class="bDate"> Date of Birth:${prophet.birthdate}</span>`;
-        birthplace.innerHTML = `<span class="bPlace"> Place of Birth:${prophet.birthplace}</span>`;
-        image.setAttribute('src', prophet.imageurl);
-        image.setAttribute('alt', `${prophet.name} ${prophet.lastname} - ${prophet.order}`);
+        h2.innerHTML = `<span class="pName">${town.name} ${town.lastname}</span>`;
+        birthdate.innerHTML = `<span class="bDate"> Date of Birth:${town.birthdate}</span>`;
+        birthplace.innerHTML = `<span class="bPlace"> Place of Birth:${town.birthplace}</span>`;
+        image.setAttribute('src', town.imageurl);
+        image.setAttribute('alt', `${town.name} ${town.lastname} - ${town.order}`);
         image.setAttribute('loading', 'lazy');
         //Create the card
         card.appendChild(h2);
