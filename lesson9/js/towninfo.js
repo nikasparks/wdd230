50a0c6f4673baea710e967baa354441f42bd451c
nikasparks <nikasparks@mac.com>
2021-11-15 03:55:09 -0700

    b

--- lesson9/js/towninfo.js
+++ lesson9/js/towninfo.js
@@ -2,34 +2,47 @@ const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
   .then(function (response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    } else {
     return response.json();
+    }
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const townInfo = jsonObject['townInfo'];
+    const townInfo = jsonObject['towns'];
 
-    const town = townInfo.filter((town) => town.name === 'Preston' || town.name === 'Fish Haven' || town.name === 'Soda Springs')
-    town.forEach(town => {
+    const only = townInfo.filter((town) => town.name === 'Preston' || town.name === 'Fish Haven' || town.name === 'Soda Springs');
+    only.forEach(town => {
         //create the card with the elements for the information
         let card = document.createElement('section');
+        let info = document.createElement('article');
         let h2 = document.createElement('h2');
-        let birthplace = document.createElement('p');
-        let birthdate = document.createElement('p');
+        let yearFounded = document.createElement('p');
+        let population = document.createElement('p');
+        let rainFall = document.createElement('p');
+        let motto = document.createElement('q');
         let image = document.createElement('img');
 
         //get info from the JSON file
-        h2.innerHTML = `<span class="pName">${town.name} ${town.lastname}</span>`;
-        birthdate.innerHTML = `<span class="bDate"> Date of Birth:${town.birthdate}</span>`;
-        birthplace.innerHTML = `<span class="bPlace"> Place of Birth:${town.birthplace}</span>`;
-        image.setAttribute('src', town.imageurl);
-        image.setAttribute('alt', `${town.name} ${town.lastname} - ${town.order}`);
+        h2.innerHTML = `<span class="tName"> ${town.name}</span>`;
+        motto.innerHTML = `<span class="tMotto">${town.motto}</span>`;
+        yearFounded.innerHTML = `<span class="year"> Founded: ${town.yearFounded}</span>`;
+        population.innerHTML = `<span class="population"> Population: ${town.currentPopulation}</span>`;
+        rainFall.innerHTML = `<span class="rainfall"> Rain Fall: ${town.averageRainfall}</span>`;
+        image.setAttribute('src', `images/${town.photo}`);
+        image.setAttribute('alt', `Image of ${town.name}`);
         image.setAttribute('loading', 'lazy');
         //Create the card
-        card.appendChild(h2);
-        card.appendChild(birthdate);
-        card.appendChild(birthplace);
+        info.appendChild(h2);
+        info.appendChild(motto);
+        info.appendChild(yearFounded);
+        info.appendChild(population);
+        info.appendChild(rainFall);
+        card.appendChild(info);
         card.appendChild(image);
         //send to html on page
-        document.querySelector('div.cards').appendChild(card);
+        document.querySelector('div.townCards').appendChild(card);
+        
     });
 });
